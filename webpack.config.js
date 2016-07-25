387dc14338c88127c64fe5edba809b5bca6c5c4d
mbellange <mbellange@gmail.com>
2016-07-25 12:42:37 +0200

    feat: ajout config webpack

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var path = require('path');
+
+module.exports = {
+  entry: [
+    path.join(__dirname, 'src', 'app.js')
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'app.bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel'
+    }]
+  }
+};
